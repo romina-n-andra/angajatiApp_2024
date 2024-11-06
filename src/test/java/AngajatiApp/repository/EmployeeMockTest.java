6209f4055323468e87d8c31308f765a5ba35ca67
Romina <162909390+romina-n-andra@users.noreply.github.com>
2024-11-07 00:08:55 +0200

    commit

--- src/test/java/AngajatiApp/repository/EmployeeMockTest.java
+++ src/test/java/AngajatiApp/repository/EmployeeMockTest.java
@@ -22,53 +22,53 @@ class EmployeeMockTest {
         employeeRepository.addEmployee(new Employee(8, "Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 5000.00));
         assertEquals(7, employeeRepository.getEmployeeList().size());
     }
-//
-//    @Test
-//    void addEmployeeWrongFormat() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8, "4lin4", "S4l4j4n", "2971205020080", DidacticFunction.ASSISTENT, 5000.00));
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeWrongCNPLonger() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8, "Alina", "Salajan", "99971205020080", DidacticFunction.ASSISTENT, 5000.00));
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeWrongSalary() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, -1.00));
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeWrongCNPShorter() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "971205020080", DidacticFunction.ASSISTENT, 5000.00));
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeZeroSalary() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8, "Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 0.00));
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeSalaryOne() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 1.00));
-//        assertEquals(7, employeeRepository.getEmployeeList().size());
-//    }
-//
-//    @Test
-//    void addEmployeeSalaryTwo() {
-//        assertEquals(6, employeeRepository.getEmployeeList().size());
-//        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 2.00));
-//        assertEquals(7, employeeRepository.getEmployeeList().size());
-//    }
+
+    @Test
+    void addEmployeeWrongFormat() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8, "4lin4", "S4l4j4n", "2971205020080", DidacticFunction.ASSISTENT, 5000.00));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeWrongCNPLonger() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8, "Alina", "Salajan", "99971205020080", DidacticFunction.ASSISTENT, 5000.00));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeWrongSalary() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, -1.00));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeWrongCNPShorter() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "971205020080", DidacticFunction.ASSISTENT, 5000.00));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeZeroSalary() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8, "Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 0.00));
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeSalaryOne() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 1.00));
+        assertEquals(7, employeeRepository.getEmployeeList().size());
+    }
+
+    @Test
+    void addEmployeeSalaryTwo() {
+        assertEquals(6, employeeRepository.getEmployeeList().size());
+        employeeRepository.addEmployee(new Employee(8,"Alina", "Salajan", "2971205020080", DidacticFunction.ASSISTENT, 2.00));
+        assertEquals(7, employeeRepository.getEmployeeList().size());
+    }
 }
